2f725ca2ceaa92360ee2f3ebfc28e9f000105f4e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-06-25 09:25:43 +0700

    Add name field

--- public/js/mod_quest.js
+++ public/js/mod_quest.js
@@ -70,6 +70,40 @@ function onSubmitAddQuest(evt) {
 function onSubmitChangeName(evt) {
     evt.preventDefault();
 
+    firebase.database().ref('user-profiles/' + signInUser.uid).once('value').then(function(userProfileEntry) {
+        var updateProfile = {
+            characterName: '',
+            strength: 0,
+            agility: 0,
+            intelligent: 0,
+        };
+        if (userProfileEntry.val())
+            updateProfile = JSON.parse(JSON.stringify(userProfileEntry));
+
+        var characterNameInput = $('#inputProfileCharacterName');
+        var characterName = characterNameInput.val();
+        updateProfile.characterName = characterName;
+
+        var updates = {};
+        updates['user-profiles/' + signInUser.uid] = updateProfile;
+
+        setDisableInputAndButton('formChangeName', true);
+        loading(true);
+        firebase.database().ref().update(updates).then(function() {
+            $('#tasksContainer').empty();
+            setDisableInputAndButton('formChangeName', false);
+            loading(false);
+        }).catch(function(error) {
+            setDisableInputAndButton('formChangeName', false);
+            loading(false);
+            // Handle Errors here.
+            if (!error || !error.code)
+                return;
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            showAlert(errorMessage);
+        });
+    });
 }
 
 function finishQuest(id) {
@@ -279,7 +313,7 @@ function refreshProfile() {
         if (userProfileEntry.val())
             updateProfile = JSON.parse(JSON.stringify(userProfileEntry));
         console.log(updateProfile);
-        $('#inputProfileCharacterName').html(updateProfile.characterName);
+        $('#inputProfileCharacterName').val(updateProfile.characterName);
         $('#profileStrValue').html(updateProfile.strength);
         $('#profileAgiValue').html(updateProfile.agility);
         $('#profileIntValue').html(updateProfile.intelligent);
--- public/service_worker.js
+++ public/service_worker.js
@@ -1,5 +1,5 @@
 var dataCacheName = 'LifeQuests-data-v1';
-var cacheName = 'LifeQuests-v1-4';
+var cacheName = 'LifeQuests-v1-5';
 var filesToCache = [
     '/',
     '/index.html',
