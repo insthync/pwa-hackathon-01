ba022f7484bfd06617d413b5b7d7c19dc0a48f59
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-06-25 08:18:04 +0700

    Finish quest done

--- public/js/mod_quest.js
+++ public/js/mod_quest.js
@@ -68,10 +68,65 @@ function onSubmitAddQuest(evt) {
 }
 
 function finishQuest(id) {
+    firebase.database().ref('user-profiles/' + signInUser.uid).once('value').then(function(userProfileEntry) {
+        var updateProfile = {
+            characterName: '',
+            strength: 0,
+            agility: 0,
+            intelligent: 0,
+        };
+        if (userProfileEntry.val())
+            updateProfile = JSON.parse(JSON.stringify(userProfileEntry));
 
+        var addStrForm = $('#inputFinishQuestStr' + id);
+        var addAgiForm = $('#inputFinishQuestAgi' + id);
+        var addIntForm = $('#inputFinishQuestInt' + id);
+
+        var addStr = addStrForm.val();
+        var addAgi = addAgiForm.val();
+        var addInt = addIntForm.val();
+
+        updateProfile.strength += Number(addStr);
+        updateProfile.agility += Number(addAgi);
+        updateProfile.intelligent += Number(addInt);
+
+        var updates = {};
+        updates['user-profiles/' + signInUser.uid] = updateProfile;
+
+        loading(true);
+        firebase.database().ref().update(updates).then(function() {
+            addStrForm.val('');
+            addAgiForm.val('');
+            addIntForm.val('');
+
+            firebase.database().ref('user-quests/' + signInUser.uid + '/' + id).remove().then(function() {
+                loading(false);
+                refreshYourQuest();
+            }).catch(function(error) {
+                loading(false);
+                // Handle Errors here.
+                if (!error || !error.code)
+                    return;
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                showAlert(errorMessage);
+            });
+        }).catch(function(error) {
+            setDisableInputAndButton('formAddQuest', false);
+            loading(false);
+            // Handle Errors here.
+            if (!error || !error.code)
+                return;
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            showAlert(errorMessage);
+        });
+    });
 }
 
 function abandonQuest(id) {
+    if (!confirm("Are you sure to abandon this quest?"))
+        return;
     loading(true);
     firebase.database().ref('user-quests/' + signInUser.uid + '/' + id).remove().then(function() {
         loading(false);
@@ -121,7 +176,7 @@ function showEmptyQuestEntryMessage(containerId, content) {
 }
 
 function clearQuestEntries(containerId) {
-    $('#' + containerId).empty();
+    $('#' + containerId).html('');
 }
 
 function addQuestEntry(containerId, id, data, actionHtml) {
@@ -147,6 +202,16 @@ function addQuestEntry(containerId, id, data, actionHtml) {
     $('#' + containerId).append(html);
 }
 
+function showFinishQuestForm(id) {
+    $('#yourQuestMenu' + id).css('display', 'none');
+    $('#yourQuestFinish' + id).css('display', 'block');
+}
+
+function hideFinishQuestForm(id) {
+    $('#yourQuestMenu' + id).css('display', 'block');
+    $('#yourQuestFinish' + id).css('display', 'none');
+}
+
 function refreshYourQuest() {
     var allQuestsRef = firebase.database().ref('user-quests/' + signInUser.uid);
     showEmptyQuestEntryMessage('yourQuestsContainer', 'You are not start any quest<br><br><a class="btn btn-lg btn-default" href="javascript:goToFindQuest()">Find Quests</a>');
@@ -159,9 +224,23 @@ function refreshYourQuest() {
                 firebase.database().ref('quests/' + userQuestData.questId).once('value').then(function(questEntry) {
                     var id = questEntry.getKey();
                     var data = JSON.parse(JSON.stringify(questEntry.val()));
-                    var actionHtml = '<a class="btn btn-md btn-success" href="javascript:finishQuest(\'' + userQuestId + '\')">Finish Quest</a>';
+                    var actionHtml = '';
+                    actionHtml += '<div id="yourQuestFinish' + userQuestId + '" class="text-left row">';
+                    actionHtml += '<div class="col-md-12"><h4>How many attributes you think you should receives by finish this quest?</h4></div>';
+                    actionHtml += '<div class="col-md-4"><label>Strength</label><input id="inputFinishQuestStr' + userQuestId + '" type="number" class="form-control" name="strength" value="0" required></div>';
+                    actionHtml += '<div class="col-md-4"><label>Agility</label><input id="inputFinishQuestAgi' + userQuestId + '" type="number" class="form-control" name="agility" value="0" required></div>';
+                    actionHtml += '<div class="col-md-4"><label>Intelligent</label><input id="inputFinishQuestInt' + userQuestId + '" type="number" class="form-control" name="intelligent" value="0" required></div>';
+                    actionHtml += '<div class="col-md-12 text-right">';
+                    actionHtml += '<br><a class="btn btn-md btn-success" href="javascript:finishQuest(\'' + userQuestId + '\')">Finish Quest</a>';
+                    actionHtml += '&nbsp;<a class="btn btn-md btn-default" href="javascript:hideFinishQuestForm(\'' + userQuestId + '\')">Cancel</a>';
+                    actionHtml += '</div>';
+                    actionHtml += '</div>';
+                    actionHtml += '<div id="yourQuestMenu' + userQuestId + '">';
+                    actionHtml += '<a class="btn btn-md btn-success" href="javascript:showFinishQuestForm(\'' + userQuestId + '\')">Finish Quest</a>';
                     actionHtml += '&nbsp;<a class="btn btn-md btn-danger" href="javascript:abandonQuest(\'' + userQuestId + '\')">Abandon</a>';
+                    actionHtml += '</div>';
                     addQuestEntry('yourQuestsContainer', 'yourQuestEntry' + userQuestId, data, actionHtml);
+                    hideFinishQuestForm(userQuestId);
                 });
             });
         }
